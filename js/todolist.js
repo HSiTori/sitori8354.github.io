70bb285489e10315c82d70341663960774135c65
sitori8354 <kmes9940723@gmail.com>
2017-01-07 23:32:10 +0800

    Update todolist.js

--- js/todolist.js
+++ js/todolist.js
@@ -9,7 +9,7 @@ function addTask(){
     var date=new Date();
     var dateString=date.getFullYear()+"年"+(date.getMonth()+1)+"月"+date.getDate()+"日"
     var time=date.getHours()+":"+date.getMinutes();
-    $("#task-list").append("<li class=\"ab\"><label><input  class='js-task-checkbox' type='checkbox'><span class='js-content'>"+value+"</span>"+","+dateString+","+time+"</label><div class=\"clear-float\"><div class=\"twoBottom\"><button class='edit'>編輯</button><br><button class='js-delete-button'>刪除</button></div></div></li>");
+    $("#task-list").append("<li class=\"ab\"><label><input  class='js-task-checkbox' type='checkbox'><span class='js-content'>"+value+"</span>"+","+dateString+","+time+"</label><div class=\"clear-float\"><br><button class='js-delete-button'>刪除</button></div></li>");
     $("#input").val("");
     updateTotalTaskNumber();
   }
@@ -20,10 +20,7 @@ function isEnterPressed(event){
     addTask();
   }
 }
-
 $("button#add-button").on("click", addTask);
-
-
 $("input#input").on("keypress", isEnterPressed);
 
 
@@ -34,45 +31,12 @@ function deleteTask(){
   updateTotalTaskNumber();
 }
 $("body").on("click", ".js-delete-button", deleteTask);
-
-
 $("#delete-completed-button").on("click", removeCompletedTasks);
                                  
-function removeCompletedTasks(){
-  $(".js-task-checkbox:checked").closest("li").remove();
-  updateTotalTaskNumber();
-}
 
 
 
-function updateTotalTaskNumber() { // #HW
+function updateTotalTaskNumber() {
   var total = $(".js-task-checkbox").length;
   $("#total-task-number").text(total);
 }
-
-function showCom(){
-  $(".js-task-checkbox").closest("li").hide();
-  $(".js-task-checkbox:checked").closest("li").show();
-}
-
-function showImcom(){
-  $(".js-task-checkbox").closest("li").show();
-  $(".js-task-checkbox:checked").closest("li").hide();
-}
-
-function showAll(){
-  $(".js-task-checkbox").closest("li").show();
-}
-$("body").on("click","#com",showCom);
-$("body").on("click","#imcom",showImcom);
-$("body").on("click","#all",showAll);
-
- 
-function editlist(){
-  var task =$(this).closest("li");
-  var ori =task.find(".js-content").text();
-  var newtask=prompt("將"+ori+"改成");
-  task.find(".js-content").text(newtask);
-}
-$("body").on("click",".edit",editlist);
- 
