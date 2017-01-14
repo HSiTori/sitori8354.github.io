20ddad518214eff59271808e8c270ebf92efb6c0
sitori8354 <kmes9940723@gmail.com>
2017-01-14 17:49:25 +0800

    Update know.js

--- js/know.js
+++ js/know.js
@@ -1,6 +1,11 @@
 
 function receive(){
   var name=$("input#knowinput").val();
-  alert("hi");
+  alert("hi"+name);
   
+}
+function check(){
+ var come=$("input#where").val();
+ alert("你好,原來你"+come);
+
 }
